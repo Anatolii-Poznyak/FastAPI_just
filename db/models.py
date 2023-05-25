53f9b073137c80d21ad3da728286f74f9535e064
Anatolii Pozniak <a13101990a@gmail.com>
2023-05-25 16:32:17 +0300

    initial commit, models created

---
+++ db/models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, String, DateTime, func, ForeignKey
+from sqlalchemy.orm import relationship
+
+from db.engine import Base
+
+
+class DBProductCategory(Base):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False, unique=True)
+
+
+class DBProduct(Base):
+    __table_name__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False, unique=True)
+    description = Column(String(511), nullable=False, unique=True)
+    price = Column(Integer, nullable=False)
+    created_at = Column(DateTime, default=func.now(), nullable=False)
+    product_category_id = Column(Integer, ForeignKey("categories.id"))
+
+    product_category = relationship(DBProductCategory)
