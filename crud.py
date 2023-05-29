1e710efd456aa70eee924019733feb77871a8ca9
Anatolii Pozniak <a13101990a@gmail.com>
2023-05-29 15:39:36 +0300

    Add create/delete to crud ( product_cat)

--- crud.py
+++ crud.py
@@ -20,3 +20,23 @@ def get_product_category(
 ):
     return db.query(models.DBProductCategory).filter(models.DBProductCategory.id == product_category_id).first()
 
+
+def delete_product_category(
+        db: Session,
+        product_category_id: int
+):
+
+    db.query(models.DBProductCategory).filter(models.DBProductCategory.id == product_category_id).delete()
+    db.commit()
+
+
+def create_product_category(
+        db: Session,
+        product_category: schemas.ProductCategoryCreate
+):
+    db_product_category = models.DBProductCategory(
+        name=product_category.name
+    )
+    db.add(db_product_category)
+    db.commit()
+    db.refresh(db_product_category)
