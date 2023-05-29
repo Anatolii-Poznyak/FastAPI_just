08793e442ac5b1d0aa2496a09d6ac7ade079619d
Anatolii Pozniak <a13101990a@gmail.com>
2023-05-29 14:57:29 +0300

    Add User and Token  Schemas

--- schemas.py
+++ schemas.py
@@ -35,3 +35,26 @@ class Product(ProductBase):
 
     class Config:
         orm_mode = True
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+
+class UserBase(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+
+class User(UserBase):
+    hashed_password: str
+
+    class Config:
+        orm_mode = True
